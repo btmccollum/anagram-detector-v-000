03dd875f94f517e6947a34ab0e01bd35fdcd0d52
Brad McCollum <btmcco8@gmail.com>
2018-08-21 00:46:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,14 +12,15 @@ class Anagram
     # binding.pry
     # if @word.split("").sort == array.each{|x| x.to_s.split("").sort}
     # binding.pry
-    # anagrams = []
-    array.map do |x|
-      if word.split("").sort == x.split("").sort
-        # anagrams << x
-        x
-      else
-         x.reject
-      end
+    # # anagrams = []
+    # array.map do |x|
+    #   if word.split("").sort == x.split("").sort
+    #     # anagrams << x
+    #     x
+    #   else
+    #      x.reject
+    #   end
+    array.map.delete_if {|x| word.split("").sort != x.split("").sort}
     end
     # anagrams
   end
