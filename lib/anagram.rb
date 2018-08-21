94f3d8eea8d3a2d2540a71e9f2d48c253c174393
Brad McCollum <btmcco8@gmail.com>
2018-08-21 00:50:01 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,12 +4,11 @@ class Anagram
   attr_accessor :word
 
   def initialize(word)
-    @word = word
+    # @word = word
     word
   end
 
   def match(array)
-    # binding.pry
     # if @word.split("").sort == array.each{|x| x.to_s.split("").sort}
     # binding.pry
     # # anagrams = []
