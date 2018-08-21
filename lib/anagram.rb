3fd46aa335295e4af9edab33edd41b1759ae4484
Brad McCollum <btmcco8@gmail.com>
2018-08-21 00:53:48 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,21 +8,38 @@ class Anagram
     # word
   end
 
-  def match(array)
-    # if @word.split("").sort == array.each{|x| x.to_s.split("").sort}
-    # binding.pry
-    # # anagrams = []
-    # array.map do |x|
-    #   if word.split("").sort == x.split("").sort
-    #     # anagrams << x
-    #     x
-    #   else
-    #      x.reject
-    #   end
+  # def match(array)
+  #   # if @word.split("").sort == array.each{|x| x.to_s.split("").sort}
+  #   # binding.pry
+  #   # # anagrams = []
+  #   # array.map do |x|
+  #   #   if word.split("").sort == x.split("").sort
+  #   #     # anagrams << x
+  #   #     x
+  #   #   else
+  #   #      x.reject
+  #   #   end
+  #   
+  # 
+  #   array.reject {|x| word.split("").sort != x.split("").sort}
+  # end
 
-    array.reject {|x| word.split("").sort != x.split("").sort}
-    # end
-    # anagrams
+
+
+def match(array)
+  # if @word.split("").sort == array.each{|x| x.to_s.split("").sort}
+  # binding.pry
+  anagrams = []
+  array.each do |x|
+    if word.split("").sort == x.split("").sort
+      anagrams << x
+    else
+       x.reject
+    end
   end
+  anagrams
+end
+  
 end
 binding.pry
+  # array.reject {|x| word.split("").sort != x.split("").sort}
