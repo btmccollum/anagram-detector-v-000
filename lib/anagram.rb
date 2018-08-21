ddba30ebad005d19d56382c6adacb5acd9daa222
Brad McCollum <btmcco8@gmail.com>
2018-08-21 00:18:03 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,17 @@
 require 'pry'
 
 class Anagram
-  attr_accessor :word
+  attr_accessor :word, :match
 
   def initialize(word)
-
+    @word = word
+    word
   end
 
+  def match(array)
+    if @word.split("").sort == match(array).split("").sort
+      puts yay
+    end
+  end
 end
   
